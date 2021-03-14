22888df30d54bfe3657a30ac8978499b4d4d2e10
Nela Strnadova <strnadova.nela@linuxmail.org>
2021-03-14 23:46:08 +0100

    start.sh skeleton

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #check yaml is supplied
-#run setup py, which will create DB based on yaml
-#run cron py, which will cron the db and see if it can run something
-#run wsgi py, which will listen to HTTP requests and create a new pipeline instance/get pipeline output or state
+#convert yaml to json
+python3 setup.py
+python3 import_json.py "$1"
+python3 cron.py &
+python3 wsgi.py &
