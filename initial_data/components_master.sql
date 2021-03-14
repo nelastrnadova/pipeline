8fb09687753bbd7d4820237113c2ef239c2f0a8a
Nela Strnadova <strnadova.nela@linuxmail.org>
2021-03-14 17:28:38 +0100

    component input 'pipeline master' parametr renamed, to be more descriptive

--- initial_data/components_master.sql
+++ initial_data/components_master.sql
@@ -2,6 +2,6 @@ CREATE TABLE components_master (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     runner VARCHAR(255) NOT NULL,
-    pipeline_fk INTEGER NOT NULL,
-    FOREIGN KEY(pipeline_fk) REFERENCES pipelines_master(id)
+    pipeline_master_fk INTEGER NOT NULL,
+    FOREIGN KEY(pipeline_master_fk) REFERENCES pipelines_master(id)
 );
