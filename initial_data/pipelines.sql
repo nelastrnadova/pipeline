c4512d8daf5fc7aba6359d1d801eda1142099ab7
Nela Strnadova <strnadova.nela@linuxmail.org>
2021-03-14 17:26:28 +0100

    pipeline instance holds its start - to be able to check if it should timeout

--- initial_data/pipelines.sql
+++ initial_data/pipelines.sql
@@ -1,6 +1,7 @@
 CREATE TABLE pipelines (
     id INTEGER PRIMARY KEY,
     state INTEGER DEFAULT 0,
+    start INTEGER DEFAULT 0,
     pipeline_master_fk INTEGER,
     FOREIGN KEY(pipeline_master_fk) REFERENCES pipelines_master(id)
 );
