e25a16d9de23616599974fa4356e105351d706fe
Nela Strnadova <strnadova.nela@linuxmail.org>
2021-03-14 17:27:15 +0100

    pipeline master holds how long to run pipeline before timeout

--- initial_data/pipelines_master.sql
+++ initial_data/pipelines_master.sql
@@ -1,5 +1,6 @@
 CREATE TABLE pipelines_master (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
+    description VARCHAR(255) NOT NULL,
+    max_runtime INTEGER DEFAULT 60
 );
