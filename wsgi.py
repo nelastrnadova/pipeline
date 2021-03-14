4732906eb4f99904e20491fa7833f3930be2773d
Nela Strnadova <strnadova.nela@linuxmail.org>
2021-03-14 17:37:00 +0100

    server is now closing connection after sending all data to client. server doesnt crash on force quit anymore

--- wsgi.py
+++ wsgi.py
@@ -21,21 +21,25 @@ def main(ip="127.0.0.1", port=8000):
 
     running = True
 
-    while running:
-        connection, client_address = sock.accept()
-        data = connection.recv(512)
-        request = data.decode().split("\r\n")
-        request_info = request[0].split(" ")
-        method = request_info[0]
-        endpoint = request_info[1][1:]
-        try:
-            request_body = json.loads(data.decode().split("\r\n\r\n")[1])
-        except JSONDecodeError:
-            request_body = {}
-        response = router(method=method, endpoint=endpoint, body=request_body)
-        connection.sendall(create_http_response(response[0], response[1]))
+    try:
+        while running:
+            connection, client_address = sock.accept()
+            data = connection.recv(512)
+            request = data.decode().split("\r\n")
+            request_info = request[0].split(" ")
+            method = request_info[0]
+            endpoint = request_info[1][1:]
+            try:
+                request_body = json.loads(data.decode().split("\r\n\r\n")[1])
+            except JSONDecodeError:
+                request_body = {}
+            response = router(method=method, endpoint=endpoint, body=request_body)
+            connection.sendall(create_http_response(response[0], response[1]))
+            connection.close()
+    except KeyboardInterrupt:
+        pass
 
-    connection.close()
+    sock.close()
 
 
 def router(method: str, endpoint: str, body: json):
