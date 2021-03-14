48a71e731df20d21831fe09b1d277555e8a3b632
Nela Strnadova <strnadova.nela@linuxmail.org>
2021-03-14 17:25:44 +0100

    component table references its pipeline

--- initial_data/components.sql
+++ initial_data/components.sql
@@ -1,6 +1,8 @@
 CREATE TABLE components (
     id INTEGER PRIMARY KEY,
     state INTEGER DEFAULT 0,
+    pipeline_fk INTEGER,
     component_master_fk INTEGER,
+    FOREIGN KEY(pipeline_fk) REFERENCES pipelines(id),
     FOREIGN KEY(component_master_fk) REFERENCES components_master(id)
 );
